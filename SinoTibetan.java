acc302618880d7a98bfc774184fc2620e9a5d038
Nathan Kelley <59635186+nathankelley@users.noreply.github.com>
2022-03-09 13:35:39 -0800

    Create SinoTibetan.java

---
+++ SinoTibetan.java
@@ -0,0 +1,11 @@
+class SinoTibetan extends Language{
+
+  public SinoTibetan(String name, int numSpeakers){
+    super(name, numSpeakers, "Asia", "subject-object-verb");
+    if (name.contains("Chinese")){
+      this.wordOrder = "subject-verb-object";
+    }
+  }
+
+  
+}
